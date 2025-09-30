a73c956bd22cb60f3c214bc8163813641265e788
Zolmex <zolmexyt@gmail.com>
2025-09-30 18:56:56 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -23,22 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
     
-    // Scroll suave
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            if (targetId === '#') return;
-            
-            const targetElement = document.querySelector(targetId);
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-    
     // Iniciar cuando el hero sea visible
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
